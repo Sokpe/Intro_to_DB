fa56b717c967b605698f0f29768b35839f2d80e9
Kay <152438149+Sokpe@users.noreply.github.com>
2025-08-21 14:55:41 +0000

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -1,6 +1,4 @@
--- Insert multiple rows into the customers table
-INSERT INTO customers (customer_id, customer_name, email, address)
-VALUES
-    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+-- Update the address of a customer
+UPDATE Customers
+SET address = '124 Happiness Ave.'
+WHERE customer_id = 1;
